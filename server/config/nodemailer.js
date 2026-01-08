0f5f3a72f71171fb21c34fbf4284b26f1d835fe5
KAVES <kavessmarthse@gmail.com>
2026-01-08 07:18:02 +0700

    Update nodemailer.js

--- server/config/nodemailer.js
+++ server/config/nodemailer.js
@@ -1,12 +1,15 @@
-import nodemailer from 'nodemailer'
+import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-    host: 'smtp-relay.brevo.com',
-    port: 587,
-    auth: {
-      user: process.env.SMTP_USER,
-      pass: process.env.SMTP_PASS,
-    }
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  secure: false, // WAJIB false untuk port 587
+  auth: {
+    user: process.env.SMTP_USER, // 98b629001@smtp-brevo.com
+    pass: process.env.SMTP_PASS, // xsmtpsib-xxxx
+  },
+  connectionTimeout: 10000, // biar gak loading abadi
+  greetingTimeout: 10000,
 });
 
 export default transporter;
